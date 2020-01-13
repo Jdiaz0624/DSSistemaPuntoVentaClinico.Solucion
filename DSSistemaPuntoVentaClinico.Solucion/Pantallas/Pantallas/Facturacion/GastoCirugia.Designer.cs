1e21e44a3c0e5cb7300cbe13e8e06e9fc031feb1
Juan Marcelino Medina Diaz <ing.juanmarcelinom.diaz@gmail.com>
2020-01-12 21:56:10 -0400

    ff

--- DSSistemaPuntoVentaClinico.Solucion/Pantallas/Pantallas/Facturacion/GastoCirugia.Designer.cs
+++ DSSistemaPuntoVentaClinico.Solucion/Pantallas/Pantallas/Facturacion/GastoCirugia.Designer.cs
@@ -28,11 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
             this.lbTitulo = new System.Windows.Forms.Label();
             this.btnCerrar = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtcantodad = new System.Windows.Forms.TextBox();
+            this.txtcomentario = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnQuitar = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Select = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.txtNumeroRegistros = new System.Windows.Forms.NumericUpDown();
+            this.lbNumeroRegistros = new System.Windows.Forms.Label();
+            this.txtNumeroPagina = new System.Windows.Forms.NumericUpDown();
+            this.lbNumeroPagina = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnRestablecer = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNumeroRegistros)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNumeroPagina)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -44,7 +70,7 @@
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1200, 70);
+            this.panel1.Size = new System.Drawing.Size(911, 70);
             this.panel1.TabIndex = 48;
             // 
             // lbTitulo
@@ -62,21 +88,266 @@
             this.btnCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnCerrar.Image = global::DSSistemaPuntoVentaClinico.Solucion.Properties.Resources.cerrar;
-            this.btnCerrar.Location = new System.Drawing.Point(1126, 5);
+            this.btnCerrar.Location = new System.Drawing.Point(837, 5);
             this.btnCerrar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCerrar.Name = "btnCerrar";
             this.btnCerrar.Size = new System.Drawing.Size(58, 50);
             this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.btnCerrar.TabIndex = 21;
             this.btnCerrar.TabStop = false;
+            this.toolTip1.SetToolTip(this.btnCerrar, "Cerrar");
             this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.btnRestablecer);
+            this.groupBox1.Controls.Add(this.btnQuitar);
+            this.groupBox1.Controls.Add(this.btnModificar);
+            this.groupBox1.Controls.Add(this.btnGuardar);
+            this.groupBox1.Controls.Add(this.txtcomentario);
+            this.groupBox1.Controls.Add(this.txtcantodad);
+            this.groupBox1.Controls.Add(this.txtDescripcion);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 78);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(888, 141);
+            this.groupBox1.TabIndex = 49;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dato de Gastos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Descripción*";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cantidad*";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Comentario";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtDescripcion.Location = new System.Drawing.Point(136, 29);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(279, 30);
+            this.txtDescripcion.TabIndex = 3;
+            // 
+            // txtcantodad
+            // 
+            this.txtcantodad.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtcantodad.Location = new System.Drawing.Point(136, 63);
+            this.txtcantodad.Name = "txtcantodad";
+            this.txtcantodad.Size = new System.Drawing.Size(279, 30);
+            this.txtcantodad.TabIndex = 4;
+            this.txtcantodad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtcantodad_KeyPress);
+            // 
+            // txtcomentario
+            // 
+            this.txtcomentario.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtcomentario.Location = new System.Drawing.Point(136, 97);
+            this.txtcomentario.Name = "txtcomentario";
+            this.txtcomentario.Size = new System.Drawing.Size(279, 30);
+            this.txtcomentario.TabIndex = 5;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardar.Location = new System.Drawing.Point(442, 27);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(141, 49);
+            this.btnGuardar.TabIndex = 6;
+            this.btnGuardar.Text = "Guardar";
+            this.toolTip1.SetToolTip(this.btnGuardar, "Guardar los datos ");
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnModificar.Enabled = false;
+            this.btnModificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnModificar.Location = new System.Drawing.Point(589, 27);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(141, 49);
+            this.btnModificar.TabIndex = 7;
+            this.btnModificar.Text = "Modificar";
+            this.toolTip1.SetToolTip(this.btnModificar, "Modificar Registros Seleccionado");
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnQuitar
+            // 
+            this.btnQuitar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnQuitar.Enabled = false;
+            this.btnQuitar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQuitar.Location = new System.Drawing.Point(736, 27);
+            this.btnQuitar.Name = "btnQuitar";
+            this.btnQuitar.Size = new System.Drawing.Size(141, 49);
+            this.btnQuitar.TabIndex = 8;
+            this.btnQuitar.Text = "Quitar";
+            this.toolTip1.SetToolTip(this.btnQuitar, "Quitar Registros Seleccionado");
+            this.btnQuitar.UseVisualStyleBackColor = true;
+            this.btnQuitar.Click += new System.EventHandler(this.btnQuitar_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 225);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(888, 282);
+            this.groupBox2.TabIndex = 50;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Listado de Tipo de Pago";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LightGray;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Select});
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(882, 253);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Select
+            // 
+            this.Select.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Select.HeaderText = "Select";
+            this.Select.Name = "Select";
+            this.Select.ReadOnly = true;
+            this.Select.Text = "Select";
+            this.Select.ToolTipText = "Select";
+            this.Select.UseColumnTextForButtonValue = true;
+            this.Select.Width = 73;
+            // 
+            // txtNumeroRegistros
+            // 
+            this.txtNumeroRegistros.BackColor = System.Drawing.Color.LightGray;
+            this.txtNumeroRegistros.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.txtNumeroRegistros.Location = new System.Drawing.Point(334, 506);
+            this.txtNumeroRegistros.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.txtNumeroRegistros.Name = "txtNumeroRegistros";
+            this.txtNumeroRegistros.Size = new System.Drawing.Size(51, 30);
+            this.txtNumeroRegistros.TabIndex = 54;
+            this.txtNumeroRegistros.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.txtNumeroRegistros.ValueChanged += new System.EventHandler(this.txtNumeroRegistros_ValueChanged);
+            // 
+            // lbNumeroRegistros
+            // 
+            this.lbNumeroRegistros.AutoSize = true;
+            this.lbNumeroRegistros.Location = new System.Drawing.Point(207, 508);
+            this.lbNumeroRegistros.Name = "lbNumeroRegistros";
+            this.lbNumeroRegistros.Size = new System.Drawing.Size(123, 25);
+            this.lbNumeroRegistros.TabIndex = 53;
+            this.lbNumeroRegistros.Text = "No.Registros";
+            // 
+            // txtNumeroPagina
+            // 
+            this.txtNumeroPagina.BackColor = System.Drawing.Color.LightGray;
+            this.txtNumeroPagina.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.txtNumeroPagina.Location = new System.Drawing.Point(118, 508);
+            this.txtNumeroPagina.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.txtNumeroPagina.Name = "txtNumeroPagina";
+            this.txtNumeroPagina.Size = new System.Drawing.Size(51, 30);
+            this.txtNumeroPagina.TabIndex = 52;
+            this.txtNumeroPagina.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.txtNumeroPagina.ValueChanged += new System.EventHandler(this.txtNumeroPagina_ValueChanged);
+            // 
+            // lbNumeroPagina
+            // 
+            this.lbNumeroPagina.AutoSize = true;
+            this.lbNumeroPagina.Location = new System.Drawing.Point(12, 510);
+            this.lbNumeroPagina.Name = "lbNumeroPagina";
+            this.lbNumeroPagina.Size = new System.Drawing.Size(103, 25);
+            this.lbNumeroPagina.TabIndex = 51;
+            this.lbNumeroPagina.Text = "No.Pagina";
+            // 
+            // btnRestablecer
+            // 
+            this.btnRestablecer.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRestablecer.Enabled = false;
+            this.btnRestablecer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRestablecer.Location = new System.Drawing.Point(442, 82);
+            this.btnRestablecer.Name = "btnRestablecer";
+            this.btnRestablecer.Size = new System.Drawing.Size(141, 49);
+            this.btnRestablecer.TabIndex = 9;
+            this.btnRestablecer.Text = "Restablecer";
+            this.toolTip1.SetToolTip(this.btnRestablecer, "Restablecer Pantalla");
+            this.btnRestablecer.UseVisualStyleBackColor = true;
+            this.btnRestablecer.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(589, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 49);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Reporte";
+            this.toolTip1.SetToolTip(this.button2, "Imprimir la hoja de gastos");
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // GastoCirugia
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
-            this.ClientSize = new System.Drawing.Size(1200, 703);
+            this.ClientSize = new System.Drawing.Size(911, 553);
+            this.Controls.Add(this.txtNumeroRegistros);
+            this.Controls.Add(this.lbNumeroRegistros);
+            this.Controls.Add(this.txtNumeroPagina);
+            this.Controls.Add(this.lbNumeroPagina);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -84,11 +355,19 @@
             this.Name = "GastoCirugia";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "GastoCirugia";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GastoCirugia_FormClosing);
             this.Load += new System.EventHandler(this.GastoCirugia_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNumeroRegistros)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNumeroPagina)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -97,5 +376,25 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lbTitulo;
         private System.Windows.Forms.PictureBox btnCerrar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtcomentario;
+        private System.Windows.Forms.TextBox txtcantodad;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnQuitar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewButtonColumn Select;
+        private System.Windows.Forms.NumericUpDown txtNumeroRegistros;
+        private System.Windows.Forms.Label lbNumeroRegistros;
+        private System.Windows.Forms.NumericUpDown txtNumeroPagina;
+        private System.Windows.Forms.Label lbNumeroPagina;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnRestablecer;
     }
 }
--- DSSistemaPuntoVentaClinico.Solucion/Pantallas/Pantallas/Facturacion/GastoCirugia.cs
+++ DSSistemaPuntoVentaClinico.Solucion/Pantallas/Pantallas/Facturacion/GastoCirugia.cs
@@ -41,6 +41,55 @@ namespace DSSistemaPuntoVentaClinico.Solucion.Pantallas.Pantallas.Facturacion
             }
         }
         #endregion
+        #region MOSTRAR EL LISTADO DE LOS GASTOS
+        private void MostrarListadoGastos(decimal IdProgramacionCirugia)
+        {
+            var MostrarListado = ObjDataFacturacion.Value.BuscaGastoCirugia(
+                new Nullable<decimal>(),
+                IdProgramacionCirugia,
+                Convert.ToInt32(txtNumeroPagina.Value),
+                Convert.ToInt32(txtNumeroRegistros.Value));
+            dataGridView1.DataSource = MostrarListado;
+        }
+        #endregion
+        #region MANTENIMIENTO DE GASTOS DE CIRUGIA
+        private void MAnGastosCirugias(string Accion)
+        {
+            try {
+                DSSistemaPuntoVentaClinico.Logica.Entidades.EntidadFacturacion.EGastosCirugia Mantenimiento = new Logica.Entidades.EntidadFacturacion.EGastosCirugia();
+
+                Mantenimiento.IdGastosCirugia = VariablesGlobales.IdMantenimiento2;
+                Mantenimiento.IdProgramacionCirugia = VariablesGlobales.IdMantenimiento;
+                Mantenimiento.Descripcion = txtDescripcion.Text;
+                Mantenimiento.Cantidad = Convert.ToInt32(txtcantodad.Text);
+                Mantenimiento.Comentario = txtcomentario.Text;
+                var MAn = ObjDataFacturacion.Value.MantenimientoGastosCirugia(Mantenimiento, Accion);
+            }
+            catch (Exception) {
+                MessageBox.Show("Error al realizar el mantenimiento", VariablesGlobales.NombreSistema, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        #endregion
+        #region RESTABLECER PANTALLA
+        private void RestablecerPantalla()
+        {
+            txtcantodad.Text = string.Empty;
+            txtcomentario.Text = string.Empty;
+            txtDescripcion.Text = string.Empty;
+            txtNumeroPagina.Value = 1;
+            txtNumeroRegistros.Value = 10;
+            btnGuardar.Enabled = true;
+            btnModificar.Enabled = false;
+            btnQuitar.Enabled = true;
+            btnRestablecer.Enabled = false;
+            txtNumeroPagina.Enabled = true;
+            txtNumeroRegistros.Enabled = true;
+            txtDescripcion.Text = string.Empty;
+            txtcantodad.Text = string.Empty;
+            txtcomentario.Text = string.Empty;
+            MostrarListadoGastos(VariablesGlobales.IdMantenimiento);
+        }
+        #endregion
         private void btnCerrar_Click(object sender, EventArgs e)
         {
             CerrarPantalla();
@@ -49,6 +98,117 @@ namespace DSSistemaPuntoVentaClinico.Solucion.Pantallas.Pantallas.Facturacion
         private void GastoCirugia_Load(object sender, EventArgs e)
         {
             SacarInformacionEmpresa(1);
+            MostrarListadoGastos(VariablesGlobales.IdMantenimiento);
+            this.dataGridView1.RowsDefaultCellStyle.BackColor = Color.LightSalmon;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.CornflowerBlue;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtDescripcion.Text.Trim()) || string.IsNullOrEmpty(txtcantodad.Text.Trim()))
+            {
+                MessageBox.Show("Has dejado campos vacios que son necesarios para guardar un nuevo registro, favor de verificar", VariablesGlobales.NombreSistema, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                VariablesGlobales.IdMantenimiento2 = 0;
+                MAnGastosCirugias("INSERT");
+                RestablecerPantalla();
+            }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtDescripcion.Text.Trim()) || string.IsNullOrEmpty(txtcantodad.Text.Trim()))
+            {
+                MessageBox.Show("Has dejado campos vacios que son necesarios para modificar el registro seleccionado, favor de verificar", VariablesGlobales.NombreSistema, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                MAnGastosCirugias("UPDATE");
+                RestablecerPantalla();
+            }
+        }
+
+        private void btnQuitar_Click(object sender, EventArgs e)
+        {
+            MAnGastosCirugias("DELETE");
+            RestablecerPantalla();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RestablecerPantalla();
+        }
+
+        private void GastoCirugia_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            switch (e.CloseReason)
+            {
+                case CloseReason.UserClosing:
+                    e.Cancel = true;
+                    break;
+            }
+        }
+
+        private void txtcantodad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            DSSistemaPuntoVentaClinico.Logica.Comunes.ValidarControles.SoloNumeros(e);
+        }
+
+        private void txtNumeroPagina_ValueChanged(object sender, EventArgs e)
+        {
+            if (txtNumeroPagina.Value < 1)
+            {
+                MessageBox.Show("El numero de paginas no puede ser menor a 1", VariablesGlobales.NombreSistema, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtNumeroPagina.Value = 1;
+                MostrarListadoGastos(VariablesGlobales.IdMantenimiento);
+            }
+            else
+            {
+                MostrarListadoGastos(VariablesGlobales.IdMantenimiento);
+            }
+        }
+
+        private void txtNumeroRegistros_ValueChanged(object sender, EventArgs e)
+        {
+            if (txtNumeroRegistros.Value < 1)
+            {
+                MessageBox.Show("El numero de registros no puede ser menor a 1", VariablesGlobales.NombreSistema, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtNumeroRegistros.Value = 10;
+                MostrarListadoGastos(VariablesGlobales.IdMantenimiento);
+            }
+            else
+            {
+                MostrarListadoGastos(VariablesGlobales.IdMantenimiento);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (MessageBox.Show("¿Quieres seleccionar este registro?", VariablesGlobales.NombreSistema, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.VariablesGlobales.IdMantenimiento2 = Convert.ToDecimal(this.dataGridView1.CurrentRow.Cells["IdGastosCirugia"].Value.ToString());
+
+                var Buscar = ObjDataFacturacion.Value.BuscaGastoCirugia(
+                    VariablesGlobales.IdMantenimiento2,
+                    VariablesGlobales.IdMantenimiento,
+                    1, 1);
+                foreach (var n in Buscar)
+                {
+                    txtDescripcion.Text = n.Descripcion;
+                    txtcantodad.Text = n.Cantidad.ToString();
+                    txtcomentario.Text = n.Comentario;
+                }
+                dataGridView1.DataSource = Buscar;
+                btnGuardar.Enabled = false;
+                btnModificar.Enabled = true;
+                btnQuitar.Enabled = true;
+                btnRestablecer.Enabled = true;
+                txtNumeroPagina.Enabled = false;
+                txtNumeroRegistros.Enabled = false;
+            }
+
         }
     }
 }
